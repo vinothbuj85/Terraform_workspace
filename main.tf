853fd4cecdc0fdaa94aa548ff6bb03adcda841b0
vinothbuj85 <42472553+vinothbuj85@users.noreply.github.com>
2026-03-25 16:08:43 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region= "us-east-1"
+}
+
+variable "ami" {
+  description =  "value"
+}
+variable "instance_type" {
+  description =  "value"
+}
+module "ec2_instance" {
+  source = "./modules/ec2_instance"
+  ami  = var.ami
+  instance_type =  var.instance_type
+}
