90e1d00bb567d7062629b418603922351aacdfaf
vinothbuj85 <42472553+vinothbuj85@users.noreply.github.com>
2026-03-25 16:01:00 +0530

    Update main.tf

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -1,3 +1,15 @@
 provider "aws" {
     region = "us-east-1"
 }
+
+variable "ami" {
+    description = "This is AMI for this instance"
+}
+
+variable "instance_type" {
+    decription ="This is an instance type, for example: t2.micro"
+}
+resource "aws_instance" "example"{
+    ami = var.ami
+    instance_type = var.instance_type
+}
