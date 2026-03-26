9f9f785a4b5bf815786c4a39ebc23684dbb1ba57
vinothbuj85 <42472553+vinothbuj85@users.noreply.github.com>
2026-03-26 17:25:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,5 +18,5 @@ variable "instance_type" {
 module "ec2_instance" {
   source = "./modules/ec2_instance"
   ami  = var.ami
-  instance_type =  lookup(instance_type,terraform.workspace,t2.micro)
+  instance_type =  lookup(var.instance_type,terraform.workspace,t2.micro)
 }
